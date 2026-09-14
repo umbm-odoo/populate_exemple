edfea40dab7f3d9f3d1bbcb04781d6ee0c9dbd6e
umbm-odoo <umbm@odoo.com>
2026-09-14 11:35:10 +0200

    fc

---
+++ populate_demo/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Populate Demo',
+    'summary': 'Sandbox blueprint to try out the populate module',
+    'category': 'Hidden/Tools',
+    'depends': ['populate', 'sale', 'account'],
+    'data': [
+        'populate/demo_partner.xml',
+        'populate/sale_invoices_5y.xml',
+    ],
+}
